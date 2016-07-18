0ce579c2787f9a1888da2cd9cd4cf7f4150eed93
Aditya Sharma <adityasma@gmail.com>
2016-07-18 13:00:31 +0530

    Update urls.py

--- simple/first/urls.py
+++ simple/first/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
 from . import views
-
+app_name='first'
 urlpatterns=[
     url(r'^$',views.details,name='details'),
     url(r'^(?P<question_id>[0-9]+)/$',views.index,name='index'),
